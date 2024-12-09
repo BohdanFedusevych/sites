bb90a079418b87e0508bf13443d81f274849d867
BohdanFedusevych <95749346+BohdanFedusevych@users.noreply.github.com>
2024-12-09 13:12:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Функція для обробки натискання кнопки
+document.getElementById('learn-more').addEventListener('click', function() {
+    alert("Дякуємо за інтерес! Ми з вами зв'яжемося найближчим часом.");
+});
